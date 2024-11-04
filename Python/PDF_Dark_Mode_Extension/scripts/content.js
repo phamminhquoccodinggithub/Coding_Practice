aa21fbf042a85fe0cb13032bf40cef3b5b27b1dd
quoc <phamminhquoccodinggithub@gmail.com>
2024-11-04 23:54:24 +0700

    Add event click to turn on and off dark mode

--- Python/PDF_Dark_Mode_Extension/scripts/content.js
+++ Python/PDF_Dark_Mode_Extension/scripts/content.js
@@ -9,6 +9,20 @@ let css = `
     background-color: white;
     mix-blend-mode: exclusion;
     z-index: 1; 
+    display: none;
 ` 
 cover.setAttribute("style", css); 
+cover.setAttribute("id", "darkModeOverlay");
 document.body.appendChild(cover);
+
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "toggle") {
+        const overlay = document.getElementById('darkModeOverlay');
+        if (overlay.style.display === 'none') {
+            overlay.style.display = 'block';
+        } else {
+            overlay.style.display = 'none';
+        }
+    }
+});
